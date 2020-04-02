279a27aa3e455c10228ecdcdbd3cab2c5a41af92
Cristea Andrei <cristeaandrei95@gmail.com>
2020-04-02 14:50:53 +0300

    feat: implemented elastic hamburger

--- src/Components/Hamburger/Hamburger.js
+++ src/Components/Hamburger/Hamburger.js
@@ -10,11 +10,16 @@ const Hamburger = ({ isDrawerOpen, setIsDrawerOpen }) => (
             setIsDrawerOpen(!isDrawerOpen);
         }}
     >
-        <div className={classnames(styles.hamburger, { [styles.open]: isDrawerOpen })}>
+        <button className={classnames(styles.hamburger, { [styles.isActive]: isDrawerOpen })} type="button">
+            <span className={styles.hamburgerBox}>
+                <span className={styles.hamburgerInner}></span>
+            </span>
+        </button>
+        {/* <div className={classnames(styles.hamburger, { [styles.open]: isDrawerOpen })}>
             <div className={classnames(styles.line, styles.top)}></div>
             <div className={classnames(styles.line, styles.middle)}></div>
             <div className={classnames(styles.line, styles.bottom)}></div>
-        </div>
+        </div> */}
         <div className={classnames(styles.closeStrip, { [styles.open]: isDrawerOpen })}></div>
     </div>
 );
